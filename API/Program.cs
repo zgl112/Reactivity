6de76116022a4cbbe05903e43999b554a472cf51
Alex Paiu <alex.paiu@reedonline.co.uk>
2020-03-13 11:36:13 +0000

    Created CRUD structure following CQRS and mediator pattern, registered new services, created code snippets.

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -15,17 +11,19 @@ namespace API
     public class Program
     {
         public static void Main(string[] args)
-        {   var host = CreateHostBuilder(args).Build();
-            
+        {
+            var host = CreateHostBuilder(args).Build();
+
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
                 try
                 {
-                   var context = services.GetRequiredService<DataContext>();
-                   context.Database.Migrate();   
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                    Seed.SeedData(context);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occured during migration!");
--- Persistance/DataContext.cs
+++ Persistance/DataContext.cs
@@ -1,29 +1,32 @@
-﻿using System;
-using Domain;
+﻿using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistance
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions options) :base(options)
+        public DataContext(DbContextOptions options) : base(options)
         {
         }
-        public DbSet<Value> Values{get; set;}
+        public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Value>()
             .HasData(
-                new Value{
-                    Id =1,
+                new Value
+                {
+                    Id = 1,
                     Name = "Value 101"
                 },
-                new Value{
-                    Id =2,
+                new Value
+                {
+                    Id = 2,
                     Name = "Value 102"
                 },
-                new Value{
-                    Id =3,
+                new Value
+                {
+                    Id = 3,
                     Name = "Value 103"
                 }
             );
