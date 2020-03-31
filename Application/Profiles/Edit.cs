079a6bfc95e9657911daa2674d42e38eea4349f1
Alex Paiu <alex.paiu@reedonline.co.uk>
2020-03-31 09:30:49 +0100

    Added editing profile bio and username feature

---
+++ Application/Profiles/Edit.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Intefaces;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+
+namespace Application.Profiles
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public string DisplayName { get; set; }
+            public string Bio { get; set; }
+        }
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.DisplayName).NotEmpty();
+                RuleFor(x => x.Bio).NotEmpty();
+            }
+        }
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly IUserAccessor _userAccessor;
+            private readonly DataContext _context;
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                this._context = context;
+                this._userAccessor = userAccessor;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users.SingleOrDefaultAsync(x =>
+                 x.UserName == _userAccessor.GetCurrentUsername());
+                user.DisplayName = request.DisplayName ?? user.DisplayName;
+                user.Bio = request.Bio ?? user.Bio;
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Unit.Value;
+
+                throw new Exception("Problem saving changes!");
+            }
+        };
+    }
+}
--- client-app/src/App/Stores/ProfileStore.ts
+++ client-app/src/App/Stores/ProfileStore.ts
@@ -100,4 +100,20 @@ export default class ProfileStore {
       });
     }
   };
+
+  @action updateProfile = async (profile: Partial<IProfile>) => {
+    try {
+      await agent.Profiles.updateProfile(profile);
+      runInAction(() => {
+        if (
+          profile.displayName !== this.rootStore.userStore.user!.displayName
+        ) {
+          this.rootStore.userStore.user!.displayName = profile.displayName!;
+        }
+        this.profile = { ...this.profile!, ...profile };
+      });
+    } catch (error) {
+      toast.error("Problem updating your profile!");
+    }
+  };
 }
